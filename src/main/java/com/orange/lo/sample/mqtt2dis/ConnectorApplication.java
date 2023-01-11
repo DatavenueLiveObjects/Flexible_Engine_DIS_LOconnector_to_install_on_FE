0ff187a07392548129a5b49801771c22d4c79f04
Szymon Lisiecki <szymon.lisiecki@likims.com>
2023-01-11 11:48:31 +0100

    revert modify

--- src/main/java/com/orange/lo/sample/mqtt2dis/ConnectorApplication.java
+++ src/main/java/com/orange/lo/sample/mqtt2dis/ConnectorApplication.java
@@ -7,31 +7,15 @@
 
 package com.orange.lo.sample.mqtt2dis;
 
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
 public class ConnectorApplication {
 
-	private static ConfigurableApplicationContext context;
-    
     public static void main(String[] args) {
-        context = SpringApplication.run(ConnectorApplication.class, args);
-    }
-    
-    public static void restart() {
-        ApplicationArguments args = context.getBean(ApplicationArguments.class);
-
-        Thread thread = new Thread(() -> {
-            context.close();
-            context = SpringApplication.run(ConnectorApplication.class, args.getSourceArgs());
-        });
-
-        thread.setDaemon(false);
-        thread.start();
+        SpringApplication.run(ConnectorApplication.class, args);
     }
 }
